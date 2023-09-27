177bc31030efc0379babcb27aa355f79196f573a
webdeveloper510 <webdeveloper510@gmail.com>
2023-09-27 16:32:16 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction1() {
+    var x = document.getElementById("myDiv1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction12() {
+    var x = document.getElementById("mysidebar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
